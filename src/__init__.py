f038952f72e3911ae84d6e8d05df2ee68c3a1670
winter_x64 <itswinterx64@gmail.com>
2023-04-01 23:13:28 +0530

    added player ground

--- src/__init__.py
+++ src/__init__.py
@@ -7,7 +7,7 @@ def MainGameFunction():
     pygame.init()
 
     #? Varible
-    size    = (1280, 720)
+    size    = (1080, 720)
     BGCOLOR = (255, 255, 255)
     
     #? Character 
@@ -23,11 +23,11 @@ def MainGameFunction():
     height = 60
 
     #* sprite velocity
-    vel = 10
+    vel = 5
     
     # Config 
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption("Zombie Zone")
+    screen = pygame.display.set_mode( size )
+    pygame.display.set_caption( "Zombie Zone" )
 
     clock = pygame.time.Clock()
 
@@ -41,13 +41,16 @@ def MainGameFunction():
 
         keys = pygame.key.get_pressed()  # This will give us a dictonary where each key has a value of 1 or 0. Where 1 is pressed and 0 is not pressed.
 
-        if keys[pygame.K_LEFT]: # We can check if a key is pressed like this
+        if keys[pygame.K_LEFT] and x > vel:  # Making sure the top left position of our character is greater than our vel so we never move off the screen.
             x -= vel
-        if keys[pygame.K_RIGHT]:
+
+        if keys[pygame.K_RIGHT] and x < 1000 - vel - width:  # Making sure the top right corner of our character is less than the screen width - its width 
             x += vel
-        if keys[pygame.K_UP]:
+
+        if keys[pygame.K_UP] and y > vel:  # Same principles apply for the y coordinate
             y -= vel
-        if keys[pygame.K_DOWN]:
+
+        if keys[pygame.K_DOWN] and y < 700 - height - vel:
             y += vel
 
         screen.blit(character, (x, y, width, height))
