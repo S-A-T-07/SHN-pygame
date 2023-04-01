4c89a4b92d1fc5b6f89bf47984f35267f1396eb8
winter_x64 <itswinterx64@gmail.com>
2023-04-01 21:06:10 +0530

    added player movement

--- src/__init__.py
+++ src/__init__.py
@@ -1,36 +1,65 @@
-# Example file showing a basic pygame "game loop"
+# Example file shoscreeng a basic pygame "game loop"
 import pygame
 
 
 def MainGameFunction():
+
     # pygame setup
     pygame.init()
-    pygame.display.set_caption("Top Down")
-    screen = pygame.display.set_mode((1280, 720))
+
+    # Varible
+    size    = (1280, 720)
+    BGCOLOR = (255, 255, 255)
+    
+    x = 50
+    y = 50
+    width = 50
+    height = 50
+    vel = 5
+    
+    
+    # walkRight = [pygame.image.load('assets/zombieGreen.png')]
+    # walkLeft = [pygame.image.load('assets/zombieGreen.png')]
+    
+    left = False
+    right = False
+    walkCount = 0
+    
+    # # Config 
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Zombie Zone")
 
     clock = pygame.time.Clock()
+
+
     running = True
-
-    y, x = 0, 0
-    speed = 5
-
     while running:
-        # poll for events
-        # pygame.QUIT event means the user clicked X to close your window
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
+        keys = pygame.key.get_pressed()  # This will give us a dictonary where each key has a value of 1 or 0. Where 1 is pressed and 0 is not pressed.
+
+        if keys[pygame.K_LEFT]: # We can check if a key is pressed like this
+            x -= vel
+        if keys[pygame.K_RIGHT]:
+            x += vel
+        if keys[pygame.K_UP]:
+            y -= vel
+        if keys[pygame.K_DOWN]:
+            y += vel
 
 
-        # fill the screen with a color to wipe away anything from last frame
-        screen.fill("purple")
 
-        # RENDER YOUR GAME HERE
+        pygame.draw.rect(screen, (255,0,0), (x, y, width, height))  #This takes: screendow/surface, color, rect 
+        pygame.display.update()
 
-        # flip() the display to put your work on screen
+        screen.fill(BGCOLOR)
+
         pygame.display.flip()
 
         clock.tick(60)  # limits FPS to 60
 
+    
     pygame.quit()
